f94d069e46512144a3ae76334fedfb8fba9439e6
Manikandan9821 <69073040+Manikandan9821@users.noreply.github.com>
2020-11-18 22:30:20 +0530

    Update app_url.dart

--- lib/utils/app_url.dart
+++ lib/utils/app_url.dart
@@ -1,9 +1,8 @@
 class AppUrl{
-    //https://api.backendless.com/D8065324-3648-BA75-FF13-B8D839BA0400/72858A17-7CA1-452C-9390-EC00F1A5F9A8/users/register
 
     static const String _url = 'https://api.backendless.com/';
-    static const String _app_id = 'D8065324-3648-BA75-FF13-B8D839BA0400/';
-    static const String _api_key = '72858A17-7CA1-452C-9390-EC00F1A5F9A8/';
+    static const String _app_id = 'Your App ID/';
+    static const String _api_key = 'Your Api Key/';
 
     static const String _base_url =  _url+_app_id+_api_key;
 
